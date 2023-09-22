1e6200e76d51938dc5423ce1759606bfed415d7a
PangAY <1623253042@qq.com>
2023-09-22 17:27:54 +0800

    补充新的环境

--- check_tsc_RL.py
+++ check_tsc_RL.py
@@ -4,40 +4,89 @@
 @Description: 测试 TSC Env 环境
 @LastEditTime: 2023-09-13 16:33:41
 '''
-import numpy as np
+import os
+import torch
 from loguru import logger
 from tshub.utils.get_abs_path import get_abs_path
 from tshub.utils.init_log import set_logger
-from stable_baselines3.common.env_checker import check_env
+
 from utils.make_tsc_env import make_env
+from utils.sb3_utils import VecNormalizeCallback, linear_schedule
+from benchmark.traffic_light.single_agent.utils.custom_models import CustomModel
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
+from stable_baselines3.common.callbacks import CallbackList, CheckpointCallback
+
 
 path_convert = get_abs_path(__file__)
-set_logger(path_convert('./'))
-
+logger.remove()
+set_logger(path_convert('./'), log_level="INFO")
 
 if __name__ == '__main__':
-    sumo_cfg = path_convert("../../sumo_envs/J1/env/J1.sumocfg")
     log_path = path_convert('./log/')
-    tsc_env_generate = make_env(
-        tls_id='J4',
-        num_seconds=3600,
-        sumo_cfg=sumo_cfg, 
-        use_gui=True,
-        log_file=log_path,
-        env_index=0,
+    model_path = path_convert('./models/')
+    tensorboard_path = path_convert('./tensorboard/')
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+    if not os.path.exists(tensorboard_path):
+        os.makedirs(tensorboard_path)
+    
+    # #########
+    # Init Env
+    # #########
+    sumo_cfg = path_convert("./TSCScenario/SumoNets/train_four_345/env/train_four_345.sumocfg")
+    params = {
+        'tls_id':'J1',
+        'num_seconds':2600,
+        'sumo_cfg':sumo_cfg,
+        'use_gui':True,
+        'log_file':log_path,
+    }
+    env = SubprocVecEnv([make_env(env_index=f'{i}', **params) for i in range(6)])
+    env = VecNormalize(env, norm_obs=False, norm_reward=True)
+
+    # #########
+    # Callback
+    # #########
+    checkpoint_callback = CheckpointCallback(
+        save_freq=10000, # 多少个 step, 需要根据与环境的交互来决定
+        save_path=model_path,
     )
-    tsc_env = tsc_env_generate()
+    vec_normalize_callback = VecNormalizeCallback(
+        save_freq=10000,
+        save_path=model_path,
+    ) # 保存环境参数
+    callback_list = CallbackList([checkpoint_callback, vec_normalize_callback])
 
-    # Check Env
-    print(tsc_env.observation_space.sample())
-    print(tsc_env.action_space.n)
-    check_env(tsc_env)
+    # #########
+    # Training
+    # #########
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    policy_kwargs = dict(
+        features_extractor_class=CustomModel,
+        features_extractor_kwargs=dict(features_dim=16),
+    )
+    model = PPO(
+                "MlpPolicy", 
+                env, 
+                batch_size=64,
+                n_steps=300, n_epochs=5, # 每次间隔 n_epoch 去评估一次
+                learning_rate=linear_schedule(1e-3),
+                verbose=True, 
+                policy_kwargs=policy_kwargs, 
+                tensorboard_log=tensorboard_path, 
+                device=device
+            )
+    model.learn(total_timesteps=3e5, tb_log_name='J1', callback=callback_list)
+    
+    # #################
+    # 保存 model 和 env
+    # #################
+    env.save(f'{model_path}/last_vec_normalize.pkl')
+    model.save(f'{model_path}/last_rl_model.zip')
+    print('训练结束, 达到最大步数.')
 
-    # Simulation with environment
-    dones = False
-    tsc_env.reset()
-    while not dones:
-        action = np.random.randint(4)
-        states, rewards, truncated, dones, infos = tsc_env.step(action=action)
-        logger.info(f"SIM: {infos['step_time']} \n+State:{states}; \n+Reward:{rewards}.")
-    tsc_env.close()
+    env.close()
