96ae16a626ff3aba421fa48f10310df784d61992
PangAY <1623253042@qq.com>
2023-09-27 16:27:20 +0800

    编写LLM

--- TSCAgent/tsc_agent.py
+++ TSCAgent/tsc_agent.py
@@ -4,6 +4,7 @@
 @Description: traffic light control LLM Agent
 @LastEditTime: 2023-09-15 20:53:32
 '''
+import numpy as np
 from typing import List
 from loguru import logger
 
@@ -11,57 +12,66 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import initialize_agent, AgentType
 from langchain.agents.tools import Tool
 from langchain.memory import ConversationTokenBufferMemory
+from langchain.chains import ConversationChain
+from langchain.prompts import ChatPromptTemplate   #导入聊天提示模板
+from langchain.chains import SimpleSequentialChain
+from langchain.chains import LLMChain    #导入LLM链。
 
 from TSCAgent.tsc_agent_prompt import SYSTEM_MESSAGE_SUFFIX
 
 class TSCAgent:
     def __init__(self, 
                  llm:ChatOpenAI, 
-                 tools:List[Tool],
-                 verbose:bool=True, action:int=0, state:float=[] ) -> None:
+                 verbose:bool=True,state:float=[] ) -> None:
         self.llm = llm # ChatGPT Model
         self.tools = [] # agent 可以使用的 tools
         self.state= state
-        self.action=action
-        for ins in tools:
-            func = getattr(ins, 'inference')
-            self.tools.append(
-                Tool(name=func.name, description=func.description, func=func)
-            )
-        
+        self.first_prompt=ChatPromptTemplate.from_template(   
+                    "What is the best name to describe \
+                     a company that makes {product}?"
+                    )
         self.memory = ConversationTokenBufferMemory(llm=self.llm, max_token_limit=2048)
-        self.agent = initialize_agent(
-            tools=self.tools, # 这里是所有可以使用的工具
-            llm=self.llm,
-            agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-            verbose=verbose,
-            memory=self.memory,
-            agent_kwargs={
-                # 'system_message_prefix': SYSTEM_MESSAGE_PREFIX,
-                'syetem_message_suffix': SYSTEM_MESSAGE_SUFFIX,
-                # 'human_message': HUMAN_MESSAGE,
-                # 'format_instructions': FORMAT_INSTRUCTIONS,
-            },
-            handle_parsing_errors="Check your output and make sure it conforms the format instructions!",
-            max_iterations=12,
-            early_stopping_method="generate",
-        )
+        self.chain_one = LLMChain(llm=llm, prompt=self.first_prompt)
+        self.chain_two = LLMChain(llm=llm, prompt=self.first_prompt)
+        self.agent =  SimpleSequentialChain(chains=[self.chain_one, self.chain_two],
+                                             verbose=True) #构建路由链 还是构建顺序链， 需要构建提示模板
+    def get_occupay(states):
+        phase_list=get_phase()
+        occupancy=states[:,1]
+        occupancy_list=np.zeros(phase_list.shape[0])
+        for i in range(0,phase_list.shape[0]):
+            occupancy_list[i]=(occupancy*phase_list[i]).sum()
+
+        occupancy_list=occupancy_list/2
+        
+        return occupancy_list
     
-    def agent_run(self, sim_step:float ):
+    def get_phase():
+        phases_4=[[1, 1, 0, 0, 0, 0, 0, 0],
+       [0, 0, 1, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 1, 0, 0],
+       [0, 0, 0, 0, 0, 0, 1, 1]]
+        
+        return np.array(phases_4)
+
+    
+    def agent_run(self, sim_step:float, action:int=0, obs:float=[]):
         """_summary_
 
         Args:
             tls_id (str): _description_
             sim_step (float): _description_
+
+        1. 现在的每个action的 movement 
+        2. 这个phase 所包含的movement 的平均占有率
+        3. 判断动作是否可行
         """
         logger.info(f"SIM: Decision at step {sim_step} is running:")
         # r = self.agent.run(
         #     f'Now you are a traffic signal light with id {tls_id}. Please analysis the efficient of the available actions you can make one by one.'
         # )
         # 需要返回上一步每个 movement 的排队长度
-        r = self.agent.run(
-            f'Please summary the current state of the intersections, and tell me which movment id is the most congestion.'
-        )
+        r = self.agent.run(action,obs)
         
         print(r)
         print('-'*10)
